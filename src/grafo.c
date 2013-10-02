f8ca39af49278a31ce5afff53ed8268992d09723
Hugo Roque <hugolnx@gmail.com>
2013-10-02 16:58:57 -0300

    casca do grafo.c

---
+++ src/grafo.c
@@ -0,0 +1,62 @@
+/***************************************************************************
+*
+*  Módulo de implementação: <abreviação>  <estrutura manipulada pelo módulo>
+*
+*  Arquivo gerado:              <nome-do-arquivo>.c
+*  Letras identificadoras:      <abreviação>
+*
+*	Autores:
+*     - <nick>: <nome>
+*     - <nick>: <nome>
+*
+*  Histórico de evolução:
+*     Versão  Autor    Data        Observações
+*     1       <nick>   <data>      <observações>
+*
+***************************************************************************/
+
+#include   <stdio.h>
+#include   <string.h>
+#include   <memory.h>
+#include   <malloc.h>
+#include   <assert.h>
+
+#define GRAFO_OWN
+#include "grafo.h"
+#undef GRAFO_OWN
+
+
+/***** Protótipos das funções encapuladas no módulo *****/
+
+   static void FuncaoEncapsulada( ) ;
+
+/*****  Código das funções exportadas pelo módulo  *****/
+
+/***************************************************************************
+*
+*  Função: GRA Função Exportada
+*  ****/
+
+   GRA_tpCondRet GRA_FuncaoExportada( )
+   {
+	   return GRA_CondRetOK;
+   }
+
+ 
+/*****  Código das funções encapsuladas no módulo  *****/
+
+/***********************************************************************
+*
+*  Função: <abreviacao> <nome da funcao por extenso>
+*
+*  Descrição:
+*    <descrição da função>
+*
+***********************************************************************/
+
+   void FuncaoEncapsulada( )
+   {
+
+   }
+
+/********** Fim do módulo de implementação: GRA Grafo direcionado **********/
