420c69bd9231016c5938ddc0c1b83ae98610c0cf
Hugo Roque <hugolnx@gmail.com>
2013-10-03 19:22:58 -0300

    criando as estruturas vertice, grafo e aresta

--- src/grafo.c
+++ src/grafo.c
@@ -20,26 +20,151 @@
 #include   <memory.h>
 #include   <malloc.h>
 #include   <assert.h>
+#include   "lista.h"
 
 #define GRAFO_OWN
 #include "grafo.h"
 #undef GRAFO_OWN
 
+/***********************************************************************
+*  Tipo de dados: GRA Vertice do grafo
+***********************************************************************/
+
+   typedef struct stVerticeGrafo {
+
+         char *nome;
+               /* Nome do vértice */
+      
+         LIS_tppLista pSucessores;
+               /* Ponteiros para as arestas originadas neste vértice */
+
+         LIS_tppLista pAntecessores;
+               /* Ponteiro para os vértices que têm arestas 
+                  direcionadas para este vértice */
+
+         void * pValor;
+               /* valor contido no vértice */
+
+   } tpVertice;
+
+/***********************************************************************
+*  Tipo de dados: GRA Aresta do grafo
+***********************************************************************/
+
+   typedef struct stArestaGrafo {
+
+         char *nome;
+               /* Nome da aresta */
+      
+         tpVertice * pVertice;
+               /* Ponteiro para o vértice destino da aresta */
+
+   } tpAresta;
+
+
+/***********************************************************************
+*  Tipo de dados: GRA Grafo
+***********************************************************************/
+
+   typedef struct stGrafo {
+
+         LIS_tppLista pOrigens;
+               /* Vértices que são origens do grafo */
+
+         LIS_tppLista pVertices;
+               /* Todos os vértices do grafo */
+      
+         tpVertice * pCorrente;
+               /* Vértice corrente */
+
+   } tpGrafo;
+
 
 /***** Protótipos das funções encapuladas no módulo *****/
 
-   static void FuncaoEncapsulada( ) ;
+   static void DestruirVertice(tpVertice *pVertice);
+   static void DestruirAresta(tpVertice *pVertice);
 
 /*****  Código das funções exportadas pelo módulo  *****/
 
-/***************************************************************************
-*
-*  Função: GRA Função Exportada
-*  ****/
+GRA_tpCondRet GRA_CriarGrafo(GRA_tppGrafo *ppGrafo,
+	void (*DestruirConteudo)(void *pConteudo))
+{
+   return GRA_CondRetGrafoVazia;
+}
 
-   GRA_tpCondRet GRA_FuncaoExportada( )
+
+
+GRA_tpCondRet GRA_DestruirGrafo(GRA_tppGrafo *ppGrafo);
+
+
+   GRA_tpCondRet GRA_InserirVertice(GRA_tppGrafo pGrafo, char *pNomeVertice, void *pValor)
    {
-	   return GRA_CondRetOK;
+      return GRA_CondRetGrafoVazia;
+   }
+
+
+
+   GRA_tpCondRet GRA_InserirArestaDoCorrentePara(GRA_tppGrafo pGrafo,
+      char *nomeAresta, char *nomeVerticeDestino)
+   {
+      return GRA_CondRetGrafoVazia;
+   }
+
+
+
+   GRA_tpCondRet GRA_DestruirVerticeCorrente(GRA_tppGrafo pGrafo)
+   {
+      return GRA_CondRetGrafoVazia;
+   }
+
+
+
+   GRA_tpCondRet GRA_DestruirArestaAdjacente(GRA_tppGrafo pGrafo, char *pNomeAresta)
+   {
+      return GRA_CondRetGrafoVazia;
+   }
+
+
+
+   GRA_tpCondRet GRA_TornarCorrenteUmaOrigem(GRA_tppGrafo pGrafo)
+   {
+      return GRA_CondRetGrafoVazia;
+   }
+
+
+
+   GRA_tpCondRet GRA_IrParaVerticeAdjacente(GRA_tppGrafo pGrafo, char *nomeVertice)
+   {
+      return GRA_CondRetGrafoVazia;
+   }
+
+
+
+   GRA_tpCondRet GRA_SeguirPelaAresta(GRA_tppGrafo pGrafo, char *nomeAresta)
+   {
+      return GRA_CondRetGrafoVazia;
+   }
+
+
+
+   GRA_tpCondRet GRA_IrParaAOrigem(GRA_tppGrafo pGrafo, char *nomeVertice)
+   {
+      return GRA_CondRetGrafoVazia;
+   }
+
+
+
+   GRA_tpCondRet GRA_ObterConteudoCorrente(GRA_tppGrafo pGrafo, void **ppValor)
+   {
+      return GRA_CondRetGrafoVazia;
+   }
+
+
+
+   GRA_tpCondRet GRA_AlterarConteudoCorrente(GRA_tppGrafo pGrafo, void *pValor)
+   {
+      return GRA_CondRetGrafoVazia;
    }
 
  
