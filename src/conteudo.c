7fa783cd72e7e0273ed9aab0e3d686ec295416de
Hugo Roque <hugolnx@gmail.com>
2013-10-06 08:55:10 -0300

    documentacao do modulo de implementacao do conteudo

--- src/conteudo.c
+++ src/conteudo.c
@@ -1,17 +1,18 @@
 /***************************************************************************
 *
-*  Módulo de implementação: <abreviação>  <estrutura manipulada pelo módulo>
+*  Módulo de implementação: CON  Conteúdo do módulo
 *
-*  Arquivo gerado:              <nome-do-arquivo>.c
-*  Letras identificadoras:      <abreviação>
+*  Arquivo gerado:              conteudo.h
+*  Letras identificadoras:      CON
 *
 *	Autores:
-*     - <nick>: <nome>
-*     - <nick>: <nome>
+*     - Hugo Roque: hg
+*     - Robert Corrêa: rc
 *
 *  Histórico de evolução:
 *     Versão  Autor    Data        Observações
-*     1       <nick>   <data>      <observações>
+*     1.0     rc       02/out/13   Criação do módulo e as operações básicas.
+*     1.1     hg       05/out/13   Checagem de erros caso o conteúdo não tenha sido criado.
 *
 ***************************************************************************/
 
@@ -34,7 +35,9 @@ typedef struct CON_stConteudo {
 
 /*****  Código das funções exportadas pelo módulo  *****/
 
-
+/***************************************************************************
+*  Função: CON Criar conteúdo
+*  ****/
 CON_tpCondRet CON_CriarConteudo(CON_tppConteudo *ppConteudo, char *pValor)
 {
 	tpConteudo *pConteudo;
@@ -50,6 +53,9 @@ CON_tpCondRet CON_CriarConteudo(CON_tppConteudo *ppConteudo, char *pValor)
 	return CON_CondRetOK;
 }
 
+/***************************************************************************
+*  Função: CON Destruir conteúdo
+*  ****/
 CON_tpCondRet CON_DestruirConteudo(CON_tppConteudo *ppConteudo)
 {
 	tpConteudo *pConteudo;
@@ -67,6 +73,9 @@ CON_tpCondRet CON_DestruirConteudo(CON_tppConteudo *ppConteudo)
 	return CON_CondRetOK;
 }
 
+/***************************************************************************
+*  Função: CON Obter valor do conteúdo
+*  ****/
 CON_tpCondRet CON_ObterValorDoConteudo(CON_tppConteudo pConteudoParm, char **ppValor)
 {
 	tpConteudo *pConteudo = pConteudoParm;
@@ -82,6 +91,9 @@ CON_tpCondRet CON_ObterValorDoConteudo(CON_tppConteudo pConteudoParm, char **ppV
 	return CON_CondRetOK;
 }
 
+/***************************************************************************
+*  Função: CON Alterar valor do conteúdo
+*  ****/
 CON_tpCondRet CON_AlterarValorDoConteudo(CON_tppConteudo pConteudoParm, char *pValor)
 {
 	tpConteudo *pConteudo = pConteudoParm;
